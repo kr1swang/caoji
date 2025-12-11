f42fedb1a25149619e4946871f4e59f7c5e1682e
Kris Wang <kris19960712@gmail.com>
2025-12-11 17:29:52 +0800

    feat: enhance doPost function to validate sheet type and log course data

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,6 +1,37 @@
-export function doPost(event: GoogleAppsScript.Events.DoPost) {
-  const payload: object = JSON.parse(event.postData?.contents || '{}')
-  const result = { method: 'doPost', ...payload }
+const sheetTypes = ['courses', 'portfolio', 'blogs'] as const
+type SheetType = (typeof sheetTypes)[number]
 
-  return ContentService.createTextOutput().setMimeType(ContentService.MimeType.JSON).setContent(JSON.stringify(result))
+type Course = {
+  id: string
+  datetime: Date
+  title: string
+  content: string
+  images: string[]
+}
+
+export function doPost(event: GoogleAppsScript.Events.DoPost) {
+  const payload = JSON.parse(event.postData?.contents || '{}')
+  const isVerified = sheetTypes.includes(payload.type)
+  if (!isVerified) throw new Error('Invalid sheet type')
+
+  const spreadsheetId = '12K9GunsmrIliM4js0AsXfUuYv44oF5TvgpFfbKK6qKs'
+  const spreadsheet = SpreadsheetApp.openById(spreadsheetId)
+  const sheet = spreadsheet.getSheetByName(payload.type as SheetType)
+  if (!sheet) throw new Error('Sheet not found')
+
+  switch (payload.type) {
+    case 'courses': {
+      const list = sheet.getDataRange().getValues()
+      Logger.log(list)
+      break
+    }
+    case 'portfolio':
+    case 'blogs': {
+      break
+    }
+  }
+
+  return ContentService.createTextOutput()
+    .setMimeType(ContentService.MimeType.JSON)
+    .setContent(JSON.stringify({ status: 'success' }))
 }
